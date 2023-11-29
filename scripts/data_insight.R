9e41a132a28c013444aad5f9c0bafc487dbedeb8
Ambroise BERTIN <ambroise.bertin@agroparistech.fr>
2023-11-29 11:05:54 +0100

    data insights update

--- scripts/data_insight.R
+++ scripts/data_insight.R
@@ -21,45 +21,26 @@ df4 <- suppressMessages(read_tsv("data/spipoll_600k_75k_202311131020.txt"))
 # merge the 4 dataframes into one
 df <- rbind(df1, df2, df3, df4)
 
-# # Gives the first 6 rows
-# print("First 6 rows")
-# print(head(df))
-# print('\n')
+# Gives the first 6 rows
+print("First 6 rows")
+print(head(df))
+print('\n')
 
-# # Gives a statistical summary of the data
-# print("Statistical summary of the data \n")
-# print(summary(df))
-# print("\n")
+# Gives a statistical summary of the data
+print("Statistical summary of the data \n")
+print(summary(df))
+print("\n")
 
-# # Gives the structure of the data
-# print("Structure of the data \n")
-# print(str(df, list.len = 0))
-# print("\n")
+# Gives the structure of the data
+print("Structure of the data \n")
+print(str(df, list.len = 0))
+print("\n")
 
-# # Gives the number of unique values in each column
-# print("Unique values in each column \n")
-# print(df %>%
-#   summarise_all(n_distinct) %>% as.vector()) 
+# Gives the number of unique values in each column
+print("Unique values in each column \n")
+print(df %>%
+  summarise_all(n_distinct) %>% as.vector()) 
 
-# # Gives the number of missing values in each column
-# print("Missing values in each column \n")
-# print(summary(sapply(df, function(x) sum(is.na(x)))))
-
-## Déclaration des variables selon leur type
-
-# # Déclare les variables numériques dans une liste
-# numerical_vars <- df[sapply(df, is.numeric)]
-# # Affiche les variables numériques
-# print(numerical_vars)
-
-# # Déclare les variables catégorielles dans une liste
-# categorical_vars <- df[sapply(df, is.character)]
-# # Affiche les variables catégorielles
-# print(categorical_vars)
-
-# Déclare les variables temporelles dans une liste
-date_vars <- df[sapply(df, is.Date)]
-
-
-# # remove specific columns
-# df %>% select(-c("commentaire", "coordonnees_GPS")) -> df
+# Gives the number of missing values in each column
+print("Missing values in each column \n")
+print(summary(sapply(df, function(x) sum(is.na(x)))))
