6f0939bc1207765f9aa4b4c2f80f9888b00d47cb
Ambroise BERTIN <ambroise.bertin@agroparistech.fr>
2023-12-04 10:56:26 +0100

    updates to geofilter

--- scripts/data_geofilter.R
+++ scripts/data_geofilter.R
@@ -1,26 +1,21 @@
+# Import the libraries
 # load packages
 if (!require("dplyr", quietly = TRUE)) {
-  install.packages("dplyr")
-}
+  install.packages("dplyr")}
+library("dplyr")
 
 if (!require("readr", quietly = TRUE)) {
-  install.packages("readr")
-}
-
-library("dplyr")
+  install.packages("readr")}
 library("readr")
 
-# read the txt data of the 4 files in the folder 'data'
-df1 <- suppressMessages(read_tsv("data/spipoll_1_200k_202311130947.txt"))
-df2 <- suppressMessages(read_tsv("data/spipoll_200k_400k_202311130949.txt"))
-df3 <- suppressMessages(read_tsv("data/spipoll_400k_200k_202311130959.txt"))
-df4 <- suppressMessages(read_tsv("data/spipoll_600k_75k_202311131020.txt"))
+library(ggplot2)
+library(gridExtra)
 
-# merge the 4 dataframes into one
-df <- rbind(df1, df2, df3, df4)
+# Import the dataset as a dataframe
+df <- read.csv("data/spipoll.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
 
 # extract the columns "coordonnees_GPS" and "code_postal"
 df <- df %>% select(coordonnees_GPS, code_postal)
 
 # display the first 6 rows of the dataframe
-head(df)
+print(head(df))
