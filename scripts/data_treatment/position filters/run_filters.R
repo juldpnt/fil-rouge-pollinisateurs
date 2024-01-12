7a553671e83132a29fefeae5848ccc818a131d1f
Ambroise BERTIN <ambroise.bertin@agroparistech.fr>
2024-01-12 10:13:13 +0100

    names updates

---
+++ scripts/data_treatment/position filters/run_filters.R
@@ -0,0 +1,8 @@
+# Run the script data_postal_filter.R
+source("data_postal_filter.R")
+
+# Run the script data_geo_filter.R
+source("data_geo_filter.R")
+
+# Run the script world_map_plot.R in the data_visualization directory
+source("data_visualization/world_map_plot.R")
