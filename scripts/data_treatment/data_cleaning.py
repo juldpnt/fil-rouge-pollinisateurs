40a80bea91a381f44ff98cc1a290c00c671e532d
Jules Dupont <jules.dupont@supagro.fr>
2024-01-13 11:36:36 +0100

    Working version after converting R > Py

--- scripts/data_treatment/data_cleaning.py
+++ scripts/data_treatment/data_cleaning.py
@@ -2,6 +2,16 @@ import pandas as pd
 from position_filters.filters import postal_code_filter, geo_filter
 import os
 
+# If save is True, the filtered dataframes will be saved
+# in the folder 'data/temporary_data'
+save = True
+
+if __name__ == "__main__":
+    # If this script is directly executed, then the working directory is
+    # the root of the project, it's for debug purpose.
+    if os.getcwd().endswith("data_treatment"):
+        os.chdir("../../")
+
 # Read the txt data of the 4 files in the folder 'data'
 df1 = pd.read_csv("data/raw_data/spipoll_1_200k_202311130947.txt", sep="\t")
 df2 = pd.read_csv("data/raw_data/spipoll_200k_400k_202311130949.txt", sep="\t")
@@ -11,10 +21,10 @@ df4 = pd.read_csv("data/raw_data/spipoll_600k_75k_202311131020.txt", sep="\t")
 # Merge the 4 dataframes into one
 df_spipoll = pd.concat([df1, df2, df3, df4])
 
-# Separate the column "coordonnees_GPS" into 2 columns "latitude" and "longitude"
+# Separate the column "coordonnees_GPS" into 2 columns "latitude" and
+# "longitude"
 df_spipoll[["latitude", "longitude"]] = df_spipoll["coordonnees_GPS"].str.split(
-    ", ", expand=True
-).astype(float)
+    ", ", expand=True).astype(float)
 df_spipoll = df_spipoll.drop(columns=["coordonnees_GPS"])
 
 # Remove variables that will never be used in the project
@@ -33,14 +43,18 @@ unused_vars = [
 ]
 df_spipoll = df_spipoll.drop(columns=unused_vars)
 
+if save:
+    df_spipoll.to_csv("data/temporary_data/spipoll.csv", index=False)
 
 # Apply filters to get which data are in metropolitan France and which are not
-df_poste = pd.read_csv("data/governmental_data/datagouv_codespostaux.csv", sep=";")
+df_poste = pd.read_csv(
+    "data/governmental_data/datagouv_codespostaux.csv",
+    sep=";")
 metropole_collections, non_metropole_collections = postal_code_filter(
     df_spipoll, df_poste
 )
 metropole_collections, non_metropole_collections = geo_filter(
-    metropole_collections, non_metropole_collections
+    metropole_collections, non_metropole_collections, save=save
 )
 
 # Once the data are filtered, we can remove the rows
@@ -50,10 +64,4 @@ rows_to_remove = df_spipoll["collection_id"].isin(
 )
 df_spipoll = df_spipoll[~rows_to_remove]
 
-df_spipoll.to_csv("data/spipoll2.csv", index=False)
-
-if __name__ == "__main__":
-    # If the script is the one being executed, then the working directory is
-    # the root of the project. Otherwise, it is the root of the package.
-    if os.getcwd().endswith("data_treatment"):
-        os.chdir("../../")
+df_spipoll.to_csv("data/spipoll.csv", index=False)
