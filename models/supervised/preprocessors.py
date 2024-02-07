43d9b2367900a0d3cd28adbb1b5c5d9d4d052466
Jules Dupont <jules.dupont@supagro.fr>
2024-02-07 13:42:21 +0100

    New version MetricsCalculator but not memory optimized

--- models/supervised/preprocessors.py
+++ models/supervised/preprocessors.py
@@ -5,7 +5,6 @@ TODO: Add description
 TODO: rassembler les calculs d'indices dans une fonction
 """
 
-from enum import verify
 from tqdm import tqdm
 import numpy as np
 
@@ -61,75 +60,59 @@ class MetricsCalculator(BaseEstimator, TransformerMixin):
         - X: pandas DataFrame, the transformed data with calculated metrics.
         """
         self.df = X
-        
+        X["_temp_indices"] = X.progress_apply(self._get_neighbours, axis=1)
+
         if self.calculate_unique_insects:
             print("Calculating unique insects...")
-            X["specific_richness"] = X.progress_apply(
-                self._calculate_unique_insects, axis=1
-            )
+            X = self.get_specific_richness(X)
 
         if self.calculate_density:
             print("Calculating density...")
-            X["density"] = X.progress_apply(self._calculate_density, axis=1)
+            X = self.get_density(X)
 
         if self.compute_collection_id_density:
             print("Calculating collection id density...")
-            X["collection_id_density"] = X.progress_apply(
-                self._compute_collection_id_density, axis=1
-            )
+            X = self.get_collection_id_density(X)
+
             print("Calculating weighted specific richness...")
-            X["weighted_specific_richness"] = (
-                X["specific_richness"] / X["collection_id_density"]
-            )
+            X = self.get_weighted_specific_richness(X)
+
+        print("Done! \n")
+        X = X.drop(columns=["_temp_indices"])
+
         return X
 
-    def _calculate_unique_insects(self, row):
+    def _get_neighbours(self, row):
         """
-        Calculate the number of unique insects within the distance for a given row.
+        Get the indices of the neighbours within the distance for a given row.
 
         Parameters:
         - row: pandas Series, a row of the input data.
 
         Returns:
-        - int, the number of unique insects within the distance.
+        - list, the indices of the neighbours within the distance.
         """
-        indices = self.tree.query_ball_point(
-            [row["latitude"], row["longitude"]], self.distance
+        lon = row["longitude"]
+        lat = row["latitude"]
+        indices = self.tree.query_ball_point([lat, lon], self.distance)
+        return indices
+
+    def get_specific_richness(self, X):
+        X["specific_richness"] = X["_temp_indices"].progress_apply(
+            lambda indices: self.df.iloc[indices]["insecte_fr"].nunique()
         )
-        return self.df.iloc[indices]["insecte_fr"].nunique()
+        return X
 
-    def _calculate_density(self, row):
-        """
-        Calculate the density of points within the distance for a given row.
+    def get_density(self, X):
+        X["density"] = X["_temp_indices"].progress_apply(len)
+        return X
 
-        Parameters:
-        - row: pandas Series, a row of the input data.
+    def get_collection_id_density(self, X):
+        X["collection_id_density"] = X["_temp_indices"].progress_apply(
+            lambda indices: self.df.iloc[indices]["collection_id"].nunique()
+        )
+        return X
 
-        Returns:
-        - int, the density of points within the distance.
-        """
-        # TODO: use the KDTree to calculate the density
-        lat = row["latitude"]
-        lon = row["longitude"]
-        return self.df[
-            (np.abs(self.df["latitude"] - lat) <= self.distance)
-            & (np.abs(self.df["longitude"] - lon) <= self.distance)
-        ].shape[0]
-
-    def _compute_collection_id_density(self, row):
-        """
-        Calculate the density of collection IDs within the distance for a given row.
-
-        Parameters:
-        - row: pandas Series, a row of the input data.
-
-        Returns:
-        - int, the density of collection IDs within the distance.
-        """
-        # TODO: use the KDTree to calculate the density
-        lat = row["latitude"]
-        lon = row["longitude"]
-        return self.df[
-            (np.abs(self.df["latitude"] - lat) <= self.distance)
-            & (np.abs(self.df["longitude"] - lon) <= self.distance)
-        ]["collection_id"].nunique()
+    def get_weighted_specific_richness(self, X):
+        X["weighted_specific_richness"] = X["specific_richness"] / X["collection_id_density"]
+        return X
