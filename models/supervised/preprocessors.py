cfd8e70a45500caf1ee0d350b6eba2520c0e2066
Jules <101271502+juldpnt@users.noreply.github.com>
2024-02-08 14:49:36 +0000

    code refactoring preprocessors

--- models/supervised/preprocessors.py
+++ models/supervised/preprocessors.py
@@ -13,6 +13,103 @@ import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.neighbors import BallTree
 
+class MetricsCalculatorNaive(BaseEstimator, TransformerMixin):
+    def __init__(
+        self,
+        distance: float,
+        calculate_unique_insects: bool = True,
+        calculate_density: bool = True,
+        compute_collection_id_density: bool = True,
+    ) -> None:
+        """
+        Initializes a MetricsCalculatorNaive object.
+
+        Args:
+            distance (float): The distance value.
+            calculate_unique_insects (bool, optional): Whether to calculate unique insects. Defaults to True.
+            calculate_density (bool, optional): Whether to calculate density. Defaults to True.
+            compute_collection_id_density (bool, optional): Whether to compute collection ID density. Defaults to True.
+        """
+        self.distance = distance
+        self.calculate_unique_insects = calculate_unique_insects
+        self.calculate_density = calculate_density
+        self.compute_collection_id_density = compute_collection_id_density
+        tqdm.pandas()
+
+    def fit(self, X: pd.DataFrame, y=None) -> "MetricsCalculatorNaive":
+        """Fit the transformer. This is a placeholder method as this transformer doesn't need to be fitted.
+
+        Args:
+            X (DataFrame): The input data.
+            y (Series, optional): The target data. Defaults to None.
+
+        Returns:
+            self
+        """
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Transforms the input data by calculating metrics.
+
+        Args:
+            X (pandas.DataFrame): The input data.
+
+        Returns:
+            pandas.DataFrame: The transformed data with calculated metrics.
+        """
+        self.df = X
+        print("Calculating metrics...")
+        for i, row in tqdm(X.iterrows(), total=X.shape[0]):
+            mask = self._get_mask(row)
+            metric = self._calculate_metrics(mask)
+            for key, value in metric.items():
+                X.loc[i, key] = value
+
+        return X
+
+    def _get_mask(self, row: pd.Series) -> pd.Series:
+        """
+        Returns a boolean mask with rows within the specified distance.
+
+        Args:
+            row (pandas.Series): The row to compare distances with.
+
+        Returns:
+            pandas.Series: A boolean mask indicating which rows are within the specified distance.
+        """
+        lat_diff = np.abs(self.df["latitude"] - row["latitude"])
+        lon_diff = np.abs(self.df["longitude"] - row["longitude"])
+        mask = (lat_diff < self.distance) & (lon_diff < self.distance)
+        return mask
+
+    def _calculate_metrics(self, mask: pd.Series) -> Dict[str, float]:
+        """
+        Calculate metrics based on the given indices.
+
+        Args:
+            indices (list): The indices of data points within the distance.
+
+        Returns:
+            dict: The calculated metrics.
+        """
+        metrics = {}
+
+        if self.calculate_unique_insects:
+            metrics["specific_richness"] = self.df[mask]["insecte_fr"].nunique()
+
+        if self.calculate_density:
+            metrics["density"] = mask.sum()
+
+        if self.compute_collection_id_density:
+            metrics["collection_id_density"] = self.df[mask]["collection_id"].nunique()
+
+        if self.calculate_unique_insects and self.compute_collection_id_density:
+            metrics["weighted_specific_richness"] = (
+                metrics["specific_richness"] / metrics["collection_id_density"]
+            )
+
+        return metrics
 
 class MetricsCalculatorTree(BaseEstimator, TransformerMixin):
     """
@@ -118,102 +215,3 @@ class MetricsCalculatorTree(BaseEstimator, TransformerMixin):
     def get_weighted_specific_richness(self, X):
         X["weighted_specific_richness"] = X["specific_richness"] / X["collection_id_density"]
         return X
-
-
-class MetricsCalculatorNaive(BaseEstimator, TransformerMixin):
-    def __init__(
-        self,
-        distance: float,
-        calculate_unique_insects: bool = True,
-        calculate_density: bool = True,
-        compute_collection_id_density: bool = True,
-    ) -> None:
-        """
-        Initializes a MetricsCalculatorNaive object.
-
-        Args:
-            distance (float): The distance value.
-            calculate_unique_insects (bool, optional): Whether to calculate unique insects. Defaults to True.
-            calculate_density (bool, optional): Whether to calculate density. Defaults to True.
-            compute_collection_id_density (bool, optional): Whether to compute collection ID density. Defaults to True.
-        """
-        self.distance = distance
-        self.calculate_unique_insects = calculate_unique_insects
-        self.calculate_density = calculate_density
-        self.compute_collection_id_density = compute_collection_id_density
-        tqdm.pandas()
-
-    def fit(self, X: pd.DataFrame, y=None) -> "MetricsCalculatorNaive":
-        """Fit the transformer. This is a placeholder method as this transformer doesn't need to be fitted.
-
-        Args:
-            X (DataFrame): The input data.
-            y (Series, optional): The target data. Defaults to None.
-
-        Returns:
-            self
-        """
-        return self
-
-    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
-        """
-        Transforms the input data by calculating metrics.
-
-        Args:
-            X (pandas.DataFrame): The input data.
-
-        Returns:
-            pandas.DataFrame: The transformed data with calculated metrics.
-        """
-        self.df = X
-
-        for i, row in tqdm(X.iterrows(), total=X.shape[0]):
-            mask = self._get_mask(row)
-            metric = self._calculate_metrics(mask)
-            for key, value in metric.items():
-                X.loc[i, key] = value
-
-        return X
-
-    def _get_mask(self, row: pd.Series) -> pd.Series:
-        """
-        Returns a boolean mask with rows within the specified distance.
-
-        Args:
-            row (pandas.Series): The row to compare distances with.
-
-        Returns:
-            pandas.Series: A boolean mask indicating which rows are within the specified distance.
-        """
-        lat_diff = np.abs(self.df["latitude"] - row["latitude"])
-        lon_diff = np.abs(self.df["longitude"] - row["longitude"])
-        mask = (lat_diff < self.distance) & (lon_diff < self.distance)
-        return mask
-
-    def _calculate_metrics(self, mask: pd.Series) -> Dict[str, float]:
-        """
-        Calculate metrics based on the given indices.
-
-        Args:
-            indices (list): The indices of data points within the distance.
-
-        Returns:
-            dict: The calculated metrics.
-        """
-        metrics = {}
-
-        if self.calculate_unique_insects:
-            metrics["specific_richness"] = self.df[mask]["insecte_fr"].nunique()
-
-        if self.calculate_density:
-            metrics["density"] = mask.sum()
-
-        if self.compute_collection_id_density:
-            metrics["collection_id_density"] = self.df[mask]["collection_id"].nunique()
-
-        if self.calculate_unique_insects and self.compute_collection_id_density:
-            metrics["weighted_specific_richness"] = (
-                metrics["specific_richness"] / metrics["collection_id_density"]
-            )
-
-        return metrics
